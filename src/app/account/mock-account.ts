ef8747cae3691eb7519e0cf08227ff0bf4496b16
rkdaredevil <rupeshkumarthakur11@gmail.com>
2019-03-24 19:43:21 +0530

    frontend commit moonmagic

---
+++ src/app/account/mock-account.ts
@@ -0,0 +1,7 @@
+import { Accountt } from './account';
+
+export const accnt: Accountt[] = [
+    {accountname: 'A S Future TRADE INDIA PVT. LTD.',
+    accountnumber: 1234432112344321, ifsccode: 'XUDV0002345', branch: 'Delhi' }
+
+];
---
+++ src/app/statefranchise/state-mock.ts
@@ -0,0 +1,35 @@
+import { StateDetails } from './state';
+
+export const state: StateDetails[] = [
+    {
+        name: 'xxxxxxxxxxx',
+        city: 'Delhi',
+        whatsapp: 7676565656,
+        paytm: 7676565656
+    },
+    {
+        name: 'xxxxxxxxxxx',
+        city: 'Odisha',
+        whatsapp: 7676565656,
+        paytm: 7676565656
+    },
+    {
+        name: 'xxxxxxxxxxx',
+        city: 'Assam',
+        whatsapp: 7676565656,
+        paytm: 7676565656
+    },
+    {
+        name: 'xxxxxxxxxxx',
+        city: 'U p',
+        whatsapp: 7676565656,
+        paytm: 7676565656
+    },
+    {
+        name: 'xxxxxxxxxxx',
+        city: 'Gujarat',
+        whatsapp: 7676565656,
+        paytm: 7676565656
+    }
+
+];
